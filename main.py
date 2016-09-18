4cfe885c495b733a9158a8c5641e3afee9dc0722
Cody Fields <contact@leethecoder.com>
2016-09-18 19:31:54 -0400

    Fixed 'clear' command causing error on Windows

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import hashlib
 import os
+import sys
+import platform
 
-# Introduction
-os.system('clear')
-print("String Hasher v1.0")
-
-# Predefined functions to hash the data
+# Predefined functions to hash the data and clear screen
 def sha1Hash():
     hash_object = hashlib.sha1(text_to_hash.encode())
     hex_dig = hash_object.hexdigest()
@@ -16,10 +14,22 @@ def md5Hash():
     hex_dig = hash_object.hexdigest()
     print('Your hash is ' + hex_dig)
 
+# This fucntion determites which OS the client is using,
+# so we don't call the wrong command to clear the screen.
+def clearScreen():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# Introduction
+clearScreen()
+print("String Hasher v1.0")
+
 # Data (text) to hash
 text_to_hash = input("Insert a string to hash: ")
 
-os.system('clear')
+clearScreen()
 print('Possible methods of hashing: MD5, SHA-1')
 while True:
     method_of_hashing = input('Use algorithm: ')
